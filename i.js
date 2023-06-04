2502c7a2a510a091044726e2b89933b023b29179
dileepk706 <dlpkmr706@gmail.com>
2023-06-04 16:45:27 +0530

    more about redux

--- i.js
+++ i.js
@@ -33,6 +33,406 @@
 // store.dispatch(placeOrder())
 // store.dispatch(placeOrder())
 // store.dispatch(placeOrder())
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data = await response.json();
+      setTodos(data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (newTodo.trim() !== '') {
+      const newTodoItem = {
+        id: Date.now(),
+        title: newTodo,
+        completed: false,
+      };
+      setTodos([...todos, newTodoItem]);
+      setNewTodo('');
+    }
+  };
+
+  const handleTodoToggle = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const handleTodoDelete = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>Todo List</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleTodoToggle(todo.id)}
+              />
+              <span className={todo.completed ? 'completed' : ''}>{todo.title}</span>
+              <button onClick={() => handleTodoDelete(todo.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={newTodo} onChange={handleInputChange} placeholder="Enter a new todo" />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data = await response.json();
+      setTodos(data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (newTodo.trim() !== '') {
+      const newTodoItem = {
+        id: Date.now(),
+        title: newTodo,
+        completed: false,
+      };
+      setTodos([...todos, newTodoItem]);
+      setNewTodo('');
+    }
+  };
+
+  const handleTodoToggle = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const handleTodoDelete = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>Todo List</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleTodoToggle(todo.id)}
+              />
+              <span className={todo.completed ? 'completed' : ''}>{todo.title}</span>
+              <button onClick={() => handleTodoDelete(todo.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={newTodo} onChange={handleInputChange} placeholder="Enter a new todo" />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data = await response.json();
+      setTodos(data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (newTodo.trim() !== '') {
+      const newTodoItem = {
+        id: Date.now(),
+        title: newTodo,
+        completed: false,
+      };
+      setTodos([...todos, newTodoItem]);
+      setNewTodo('');
+    }
+  };
+
+  const handleTodoToggle = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const handleTodoDelete = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>Todo List</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleTodoToggle(todo.id)}
+              />
+              <span className={todo.completed ? 'completed' : ''}>{todo.title}</span>
+              <button onClick={() => handleTodoDelete(todo.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={newTodo} onChange={handleInputChange} placeholder="Enter a new todo" />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data = await response.json();
+      setTodos(data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (newTodo.trim() !== '') {
+      const newTodoItem = {
+        id: Date.now(),
+        title: newTodo,
+        completed: false,
+      };
+      setTodos([...todos, newTodoItem]);
+      setNewTodo('');
+    }
+  };
+
+  const handleTodoToggle = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const handleTodoDelete = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>Todo List</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleTodoToggle(todo.id)}
+              />
+              <span className={todo.completed ? 'completed' : ''}>{todo.title}</span>
+              <button onClick={() => handleTodoDelete(todo.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={newTodo} onChange={handleInputChange} placeholder="Enter a new todo" />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data = await response.json();
+      setTodos(data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (newTodo.trim() !== '') {
+      const newTodoItem = {
+        id: Date.now(),
+        title: newTodo,
+        completed: false,
+      };
+      setTodos([...todos, newTodoItem]);
+      setNewTodo('');
+    }
+  };
+
+  const handleTodoToggle = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const handleTodoDelete = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>Todo List</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleTodoToggle(todo.id)}
+              />
+              <span className={todo.completed ? 'completed' : ''}>{todo.title}</span>
+              <button onClick={() => handleTodoDelete(todo.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={newTodo} onChange={handleInputChange} placeholder="Enter a new todo" />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
 
 
 const redux=require('redux')
